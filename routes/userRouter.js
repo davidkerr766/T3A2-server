a160661a0e7537a222efb7b0bac1aa2941f30c9e
davidkerr766 <david.kerr766@outlook.com>
2020-07-15 09:14:35 +1000

    user login route added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,5 +9,12 @@ router.get('/', async (req, res) => {
   res.send(users);
 });
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and password are required"})
+  }
+})
 
 module.exports = router;
