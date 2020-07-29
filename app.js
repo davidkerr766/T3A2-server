260075869e0fce726ba0c9b5934a80e2894d8fcb
davidkerr766 <david.kerr766@outlook.com>
2020-07-30 09:21:02 +1000

    all tests working

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors')
-
 const app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@cluster0.5w7cn.mongodb.net/dev?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+let database
+if (process.env.ENV == "test") {
+    database = "test"
+} else {
+    database = "prod"
+}
+mongoose.connect(`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@cluster0.5w7cn.mongodb.net/${database}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -25,4 +30,4 @@ app.use('/api/images', require('./routes/awsRouter'));
 app.use('/api/blogs', require('./routes/blogRouter'))
 app.use('/api/recipes', require('./routes/recipeRouter'))
 
-module.exports = app
+module.exports = {app, db}
